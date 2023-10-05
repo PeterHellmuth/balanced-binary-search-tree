3697f0c23550dfdf2c5b93cff6a79cf6f8df1ccc
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-10-05 12:46:17 -0500

    Add inorderSuccessor method for delete method use

--- script.js
+++ script.js
@@ -12,7 +12,82 @@ class Tree {
   }
 
   insert(data) {
-    this.root = this.insertRec(this.root, data);
+    if (!this.contains(data)) {
+      this.root = this.insertRec(this.root, data);
+    }
+  }
+
+  delete(data, root = this.root) {
+    if (root == null) {
+      return false;
+    }
+
+    if (root.data == data) {
+      if (root.left == null && root.right == null) {
+        root.data = null;
+        return true;
+      } else if (root.left != null) {
+        root.data = root.left.data;
+        root.left = null;
+        return false;
+      } else if (root.right != null) {
+        root.data = root.right.data;
+        root.right = null;
+        return false;
+      }
+    }
+    if (this.delete(data, root.left)) {
+      root.left = null;
+      return false;
+    }
+    if (this.delete(data, root.right)) {
+      root.right = null;
+      return false;
+    }
+  }
+
+  inorderSuccessor(node) {
+    if (node.right != null) {
+      let current = node.right;
+
+      while (current.left != null) {
+        current = current.left;
+      }
+      return current;
+    } else {
+      let inorderSucc = null;
+      let currentRoot = this.root;
+
+      while (currentRoot != null) {
+        //console.log(currentRoot.data);
+        if (node.data < currentRoot.data) {
+          inorderSucc = currentRoot;
+          currentRoot = currentRoot.left;
+        } else if (node.data > currentRoot.data) {
+          currentRoot = currentRoot.right;
+        } else {
+          break;
+        }
+      }
+
+      return inorderSucc;
+    }
+  }
+
+  contains(data, root = this.root) {
+    if (root) {
+      if (root.data == data) {
+        return true;
+      } else {
+        if (root.left || root.right) {
+          return (
+            this.contains(data, root.left) || this.contains(data, root.right)
+          );
+        }
+      }
+    }
+
+    return false;
   }
 
   insertRec(root, data) {
@@ -53,14 +128,14 @@ function buildTreeRec(sortedArr, start = 0, end = sortedArr.length) {
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
+  if (node == null) {
     return;
   }
-  if (node.right !== null) {
+  if (node.right != null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  if (node.left !== null) {
+  if (node.left != null) {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
@@ -69,6 +144,7 @@ let aTree = new Tree(
   buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 );
 
-aTree.insert(5667);
+console.log(aTree.inorderSuccessor(aTree.root.right.right));
+//console.log(aTree.contains(7));
 
 prettyPrint(aTree.root);
